56ea63a84b04ca0ef5fc4bc95a3ea861f9dd91e9
CondoRHxH <fahdcondor04@gmail.com>
2026-03-01 00:37:10 +0000

    Changes

--- src/EndUsers/AddBook.java
+++ src/EndUsers/AddBook.java
@@ -7,28 +7,29 @@ public class AddBook implements IOOperation {
 	@Override
 	public void oper(database database, User user) {
 		Scanner s = new Scanner(System.in);
+	
 		Book book = new Book();
 		
 		System.out.println("Enter The Book Name : ");
-		book.setName(s.next());
+		book.setName(s.nextLine());
 		
 		System.out.println("Enter The Book Author : ");
-		book.setAuthor(s.next());
+		book.setAuthor(s.nextLine());
 		
 		System.out.println("Enter The Book Publisher : ");
-		book.setPublisher(s.next());
+		book.setPublisher(s.nextLine());
 		
 		System.out.println("Enter The Book Collection adress : ");
-		book.setPublisher(s.next());
+		book.setAdress(s.nextLine());
 		
 		System.out.println("Enter The Quantite : ");
-		book.setQty(s.nextInt());
+		book.setQty(Integer.parseInt(s.nextLine()));
 		
 		System.out.println("Enter The Price : ");
-		book.setPrice(s.nextInt());
+		book.setPrice(Integer.parseInt(s.nextLine()));
 		
 		System.out.println("Enter Borrowing pieces : ");
-		book.setBrwcopies(s.nextInt());
+		book.setBrwcopies(Integer.parseInt(s.nextLine()));
 		
 		database.AddBook(book);
 		System.out.print("Book Added Succefully");
