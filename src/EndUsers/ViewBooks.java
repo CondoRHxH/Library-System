c73fe7d8088f93e3194b650bc6978bafbb2e1442
CondoRHxH <fahdcondor04@gmail.com>
2026-03-09 20:30:17 +0000

    NewBranch

--- src/EndUsers/ViewBooks.java
+++ src/EndUsers/ViewBooks.java
@@ -8,10 +8,12 @@ public class ViewBooks implements IOOperation{
 	public void oper(database database, User user) {
 		ArrayList<Book> books = database.getAllBooks();
 		
-		System.out.println("Name\tAuthor\tPublisher\tCollection location Adress\tStatus\tQty\tPrice"+"\tBorrowin copies");
+		System.out.println("Name\t\tAuthor\tPublisher\tCollection location Adress\tStatus\tQty\tPrice"+"\tBorrowin copies");
 		
 		for(Book b : books) {
-			System.out.println(b.getName()+"\t"+b.getPublisher()+"\t"+b.getAdress()+"\t"+b.getStatus()+"\t"+b.getQty()+"\t"+b.getPrice()+"\t"+b.getBrwcopies());
+			//System.out.println(b.getName()+"\t\t"+b.getAuthor()+"\t\t"+b.getPublisher()+"\t\t"+b.getAdress()+"\t"+b.getStatus()+"\t"+b.getQty()+"\t"+b.getPrice()+"\t"+b.getBrwcopies());
+			System.out.println(b.getName()+"\t\t"+b.getAuthor()+"\t\t"+b.getPublisher()+"\t\t"+b.getAdress()+"\t"+b.getQty()+"\t"+b.getPrice()+"\t"+b.getBrwcopies());
+		
 		}
 		System.out.println();
 		user.menu(database, user);
