0ff3a9402097a8c75625d37cb2111bae250a87c8
CondoRHxH <fahdcondor04@gmail.com>
2026-03-13 00:51:18 +0000

    Borrowing Class

---
+++ src/EndUsers/Borrowing.java
@@ -0,0 +1,66 @@
+package EndUsers;
+
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
+
+public class Borrowing {
+
+	LocalDate start;
+	LocalDate finish;
+	
+	int daysleft;
+	Book book;
+	User user;
+	
+	DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+	
+	public Borrowing(Book book, User user) {
+		start = LocalDate.now();
+		finish = start.plusDays(14);
+		Period period = Period.between(start,finish);
+		daysleft = period.getDays();
+		this.book = book;
+		this.user = user;
+	}
+	
+	public Borrowing(LocalDate start, LocalDate finish, Book book, User user) {
+		this.start = start;
+		this.finish = finish;
+		this.daysleft = Period.between(start, finish).getDays();
+		this.book = book;
+		this.user = user;
+	}
+	
+	public String getStart() {
+		return formatter.format(start);
+	}
+	
+	public String getFinish() {
+		return formatter.format(finish);
+	}
+	
+	public int getDaysdLeft() {
+		return Period.between(start, finish).getDays();
+	}
+	
+	public Book getBook() {
+		return book;
+	}
+	
+	public User getUser() {
+		return user;
+	}
+	
+	public void setBook(Book book) {
+		this.book = book;
+	}
+	
+	public void setUser(User user) {
+		this.user = user;
+	}
+	
+	public String toString() {
+		return "Borrowing Time :"+start+"\nExpiry Date :"+finish+"\nDays left : "+daysleft;
+	}
+}
